0ec4ba7fbf9725f88b269d1246586610477b4d5b
Yi (Brian) Shan <yi.shan@nyu.edu>
2019-12-22 13:42:35 -0500

    update models' likelihood function

--- BrianProject.py
+++ BrianProject.py
@@ -6,79 +6,140 @@ import arch
 from statsmodels.tsa import arima_model
 from scipy.optimize import minimize
 # %%
-f = '/mnt/d/study/2019/2707_Time_Series_Analysis_Statistical_Arbitrage/project/VIX.csv'
+f = '/mnt/d/study/2019/2707_Time_Series_Analysis_Statistical_Arbitrage/project/mine/VIX.csv'
 data = pd.read_csv(f, index_col=0)
 data.head(5)
 # %%
-plt.plot(data['Close'])
+plt.plot(np.array(data['Close']))
 plt.show()
-
 # %%
 close = data[['Close']]
-train_idx = data.index.get_loc('2013-12-31')+1
-val_idx = data.index.get_loc('2017-12-29')+1
-close_train = close[:train_idx]
-print(close_train.tail(2))
-close_val = close[train_idx:val_idx]
-print(close_val.tail(2))
-close_test = close[val_idx:]
-print(close_test.tail(2))
+# train_idx = data.index.get_loc('2013-12-31')+1
+# val_idx = data.index.get_loc('2017-12-29')+1
+# close_train = close[:train_idx]
+# print(close_train.tail(2))
+# close_val = close[train_idx:val_idx]
+# print(close_val.tail(2))
+# close_test = close[val_idx:]
+# print(close_test.tail(2))
+#%%
+def normal_lhdfn(mu, var):
+    return 0.5 * np.sum(np.log(var)) + 0.5*np.sum(mu * mu/var)
 # %%
-# ARIMA(p,d,q)
-best_aic = np.inf
-best_order = None
-best_mdl = None
-pq_rng = range(5) # [0,1,2,3,4]
-d_rng = range(2) # [0,1]
-for i in pq_rng:
-    for d in d_rng:
-        for j in pq_rng:
-            try:
-                tmp_mdl = arima_model.ARIMA(close_train, order=(i,d,j)).fit(method='mle', trend='nc')
-                tmp_aic = tmp_mdl.aic
-                if tmp_aic < best_aic:
-                    best_aic = tmp_aic
-                    best_order = (i, d, j)
-                    best_mdl = tmp_mdl
-            except: continue
-print(f"best model (p,d,q): {best_order} with aic: {best_aic}" )
-# %%
-# ARIMA(1,1,1)-GARCH(1,1)
-close_train_1Diff = close_train.diff()[1:]
-
+# ARIMA(1,1,1)
+def ARIMA111(Xt, days):
+    Xt = Xt[1:] - Xt[:-1]
+    def ARIMA111_lhdfn(params):
+        alpha1 = params[0]
+        beta1 = params[1]
+        variable = Xt[1:] - alpha1 * Xt[:-1]
+        return normal_lhdfn(variable, 1+beta1*beta1)
+    params = [0.2,0.2]
+    res = minimize(ARIMA111_lhdfn, params, method = 'nelder-mead', 
+            tol = 1e-8, options = {'disp': True, 'maxiter': 10000})
+    return res
 # %%
 # nabla Pt = kappa + beta nabla P_{t-1} + a_t + theta a_{t-1}
 # a_t = sigma_t epsilon_t 
 # sigma_t^2 = alpha_0 + alpha1 a_{t-1}^2 + alpha2 sigma_{t-1}^2
-Xt = np.array(close_train_1Diff['Close'])
-def ARIMA111_GARCH11_lhdfn(params):
-    kappa = params[0] 
-    beta = params[1]
-    theta = params[2]
-    sigma = params[3]
-    alpha0 = params[4]
-    alpha1 = params[5]
-    alpha2 = params[6]
-    errorSum = Xt[1:] - kappa  - beta * Xt[:-1]
-    at = np.zeros(len(Xt))
-    at[0] = errorSum[0]
-    sigmat_squared = np.zeros(len(Xt))
-    sigmat_squared[0] = sigma * sigma
-    for i in range(1,len(Xt)):
-        sigmat_squared[i] = alpha0 + alpha1 * at[i-1]*at[i-1] + alpha2 * sigmat_squared[i-1]
-        at[i] = errorSum[i-1] - theta * at[i-1]
-    vart = np.zeros(len(Xt) - 1)
-    for i in range(0,len(vart)):
-        vart[i] = sigmat_squared[i+1] + theta * theta * sigmat_squared[i]
-    return 0.5 * np.sum(np.log(vart)) + 0.5*np.sum(errorSum * errorSum/vart)
+def ARIMA111_GARCH11(Xt):
+    Xt = Xt[1:] - Xt[:-1]
+    def ARIMA111_GARCH11_lhdfn(params):
+        kappa = params[0] 
+        beta = params[1]
+        theta = params[2]
+        sigma = params[3]
+        alpha0 = params[4]
+        alpha1 = params[5]
+        alpha2 = params[6]
+        errorSum = Xt[1:] - kappa  - beta * Xt[:-1]
+        at = np.zeros(len(Xt))
+        at[0] = errorSum[0]
+        sigmat_squared = np.zeros(len(Xt))
+        sigmat_squared[0] = sigma * sigma
+        for i in range(1,len(Xt)):
+            sigmat_squared[i] = alpha0 + alpha1 * at[i-1]*at[i-1] + alpha2 * sigmat_squared[i-1]
+            at[i] = errorSum[i-1] - theta * at[i-1]
+        vart = np.zeros(len(Xt) - 1)
+        for i in range(0,len(vart)):
+            vart[i] = sigmat_squared[i+1] + theta * theta * sigmat_squared[i]
+        return normal_lhdfn(errorSum,vart)
+    params = [np.mean(Xt),0.3, 0.3, np.std(Xt), 0.2,0.2,0.2]
+    res = minimize(ARIMA111_GARCH11_lhdfn, params, method = 'nelder-mead', 
+            tol = 1e-8, options = {'disp': True, 'maxiter': 10000})
+    return res
+
 
 # %%
-params = [np.mean(Xt),0.3, 0.3, np.std(Xt), 0.2,0.2,0.2]
-res = minimize(ARIMA111_GARCH11_lhdfn, params, method = 'nelder-mead', 
-        tol = 1e-5, options = {'disp': True, 'maxiter': 10000})
-print(res.x)
+def ARIMA111_GJRGARCH11(Xt):
+    Xt = Xt[1:] - Xt[:-1]
+    def ARIMA111_GJRGARCH11_lhdfn(params):
+        kappa = params[0] 
+        beta = params[1]
+        theta = params[2]
+        sigma = params[3]
+        alpha0 = params[4]
+        alpha1 = params[5]
+        alpha2 = params[6]
+        gamma1 = params[7]
+        errorSum = Xt[1:] - kappa  - beta * Xt[:-1]
+        at = np.zeros(len(Xt))
+        at[0] = errorSum[0]
+        sigmat_squared = np.zeros(len(Xt))
+        sigmat_squared[0] = sigma * sigma
+        for i in range(1,len(Xt)):
+            sigmat_squared[i] = alpha0 + alpha1 * at[i-1]*at[i-1] + alpha2 * sigmat_squared[i-1] 
+            if at[i-1] > 0:
+                sigmat_squared[i] += gamma1 * at[i-1] * at[i-1]
+            at[i] = errorSum[i-1] - theta * at[i-1]
+        vart = np.zeros(len(Xt) - 1)
+        for i in range(0,len(vart)):
+            vart[i] = sigmat_squared[i+1] + theta * theta * sigmat_squared[i]
+        return normal_lhdfn(errorSum,vart)
+    params = [np.mean(Xt),0.3, 0.3, np.std(Xt), 0.2,0.2,0.2]
+    res = minimize(ARIMA111_GJRGARCH11_lhdfn, params, method = 'nelder-mead', 
+            tol = 1e-8, options = {'disp': True, 'maxiter': 10000})
+    return res
 # %%
+def heston(Xt):
+    def heston_lhdfn(params):
+        lam = params[0]
+        mu = params[1]
+        sigma = params[2]
+        dXt = Xt[1:] - Xt[:-1]
+        variable = dXt + lam * (Xt[:-1] - mu) 
+        return normal_lhdfn(variable, sigma**2)
+    params = [0.2, np.mean(Xt), np.var(Xt)]
+    res = minimize(heston_lhdfn, params, method = 'nelder-mead', 
+            tol = 1e-8, options = {'disp': True, 'maxiter': 10000})
+    return res
+# %%
+def OU(Xt):
+    def OU_lhdfn(params):
+        lam = params[0]
+        mu = params[1]
+        sigma = params[2]
+        lnXt = np.log(Xt)
+        lnXt_diff = lnXt[1:] - lnXt[:-1]
+        variable = lnXt_diff + lam* (lnXt[:-1] - mu)
+        return normal_lhdfn(variable, sigma**2)
+    params = [0.2, np.mean(Xt), np.var(Xt)]
+    res = minimize(OU_lhdfn, params, method = 'nelder-mead', 
+            tol = 1e-8, options = {'disp': True, 'maxiter': 10000})
+    return res
+# %%
+f_future = '/mnt/d/study/2019/2707_Time_Series_Analysis_Statistical_Arbitrage/project/mine/VIX_Future.csv'
+data_future = pd.read_csv(f_future, index_col=0)
+data_future.head(5)
 
+# %%
+date = data_future.index[0]
+t_date = close.index.get_loc(date)
+best_order, best_mdl, best_aic = ARIMA111(np.array(close[:t_date]))
 
+# %%
+print(close.iloc[t_date])
+for i in range(5):
+    print(best_mdl.forecast(steps = 5))
 
 # %%
--- model.md
+++ model.md
@@ -1,9 +1,9 @@
-1. ARIMA(p,d,q)
+1. ARIMA(1,1,1)
 $$
-	\nabla^d X_t = \alpha_1 \nabla^d X_{t-1} + \cdots + \alpha_p \nabla^d X_{t-p} + Z_t + \beta_1 Z_{t-1}+ \cdots + \beta_q Z_{t-q}
+	\nabla X_t = \alpha_1 \nabla X_{t-1}  + Z_t + \beta_1 Z_{t-1}
 $$
 
-2. ARIMA(1,1,1)-GARCH(1,1)
+1. ARIMA(1,1,1)-GARCH(1,1)
 
 Let $P_t = \nabla^1 X_t$
 $$
@@ -13,3 +13,22 @@ $$
 a_t = \sigma_t \varepsilon_t \qquad \qquad \sigma_t^2 = \alpha_0 + \alpha_1 a_{t-1}^2 + \alpha_2 \sigma_{t-1}^2
 $$
 Hence, $P_t - \kappa - \beta_1P_{t-1} \sim N(0, \sigma_t^2 + \theta^2 \sigma_{t-1}^2)$.
+
+3. Heston model
+$$
+dVIX_t  = \lambda (\mu  - VIX_t) * dt + \sigma \times \sqrt{VIX_t} * dW_t
+$$
+Hence, $\Delta VIX_t - \lambda (\mu - VIX_t) \sim N(0, \sigma^2\sqrt{VIX_t})$
+
+
+4. OU process
+$$
+d \ln VIX_t = \lambda * (\mu - \ln VIX_t) * dt + \sigma * dW_t
+$$
+
+5. GJR-GARCH model
+$$
+\sigma_t^2 = \alpha_0 + \alpha_1 a_{t-1}^2 + \gamma_1 I_{a_{t-1}}a_{t-1}^2 + \alpha_2 \sigma_{t-1}^2
+$$
+
+Note that $I_{a_{t-1}} = \begin{cases} 1 & \text{if } a_{t-1} > 0 \\ 0 &\text{otherwise} \end{cases}$ which is the inverse of regular GJR-GARCH
